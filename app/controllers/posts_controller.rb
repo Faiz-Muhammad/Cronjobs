cac8dab4cb352caeecaabec5c158214c50a79ca0
UsmanAzmat300 <d.usmanazmat@gmail.com>
2019-04-18 19:31:16 +0500

    data_saving_in_db

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,12 +9,14 @@ class PostsController < ApplicationController
   end
 
   def create
-    link.each_with_index do |l,index|
-      post_new_params = Hash.new
-      post_new_params.merge!({"link" => "#{l}", "description" => "#{description[index]}"})
-      @post = current_user.posts.create!(post_new_params)
-      calculate_posting_time(start_time,interval,time_gap,delete_time,@post, @pages)
-    end
+    @pages=[]
+    @pages << Page.first
+     # link.each_with_index do |l,index|
+       post_new_params = Hash.new
+       post_new_params.merge!({"link" => "#{params["post"]['link']}", "description" => "#{params["post"]['description']}"})
+      @post = current_user.posts.create!(post_params)
+      calculate_posting_time(params["post"]['start_time'],params["post"]['interval'],params["post"]['time_gap'],params["post"]['delete_time'],@post, @pages)
+    # end
     # @post = Post.new(post_params)
     # @post.user = current_user
     # @post.save
@@ -29,19 +31,20 @@ class PostsController < ApplicationController
   end
 
   def calculate_posting_time(start_time, interval, time_gap, delete_time, post, pages)
+    scheduled_publish_time = 0
     pagespost_params = Hash.new
-    pages.each_with_index do |page|
+    pages.each_with_index do |page,index|
       if index != 0
-        scheduled_publish_time += interval
+        scheduled_publish_time += interval.to_i
       end
-      scheduled_publish_time += start_time + time_gap
+      scheduled_publish_time += start_time.to_i + time_gap.to_i
       if !delete_time.nil?
-        delete_post_time = scheduled_publish_time + delete_time
+        delete_post_time = scheduled_publish_time + delete_time.to_i
       else
         delete_post_time = 0
       end
-      pagespost_params.merge!("#{index}" => {"page_id" => "#{page.id}", "post_id" => "#{post.id}",
-         "scheduled_published_time" => "#{scheduled_publish_time}", "delete_post_time" => "#{delete_post_time}"})
+      pagespost_params = {"page_id" => "#{page.id}", "post_id" => "#{post.id}",
+         "scheduled_published_time" => "#{scheduled_publish_time}", "delete_post_time" => "#{delete_post_time}"}
       @pagespost = current_user.pagesposts.create!(pagespost_params)
     end
   end
