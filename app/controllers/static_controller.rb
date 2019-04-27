ee7c398daeb30b0e8453ac023ebc5b8d84d4dfd2
UsmanAzmat300 <d.usmanazmat@gmail.com>
2019-04-27 21:34:49 +0500

    static_controller

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,14 @@
+class StaticController < ApplicationController
+  def privacy_policy
+
+  end
+  def terms_condition
+
+  end
+  def faqs
+
+  end
+  def payment_options
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html 
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "homes#index"
   resources :users
   get '/myaccounts', to: 'pages#index'
   resources :pages
   resources :posts
-  
+
+  get '/privacy_policy', to: 'static#privacy_policy'
+  get '/terms_condition', to: 'static#terms_condition'
+  get '/faqs', to: 'static#faqs'
+  get '/payment_options', to: 'static#payment_options'
+
 end
